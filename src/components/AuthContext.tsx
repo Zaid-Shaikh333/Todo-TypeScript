00dc3dd474e2a2a1c147209e504b2e8ad63d958e
Zaid Shaikh <zaidshaikh@gofynd.com>
2023-06-10 12:31:18 +0530

    Added Logic for checking registered user from JSON Mock + Setting up JWT Token & storing in LocalStorage

--- src/components/AuthContext.tsx
+++ src/components/AuthContext.tsx
@@ -1,25 +1,26 @@
 import React, { createContext, useState, ReactNode } from 'react';
-const jwt = require('jsonwebtoken');
-
-
-interface User {
-  username: string;
-  name: string;
-  todos: Todo[];
-  token: string | null;
-}
-
-interface AuthContextType {
-  user: User | null;
-  login: (username: string, password: string) => void;
-  logout: () => void;
-}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import jwt from 'jsonwebtoken';
+import usersData from '../mockdata/mock.json';
 
 interface Todo {
   id: number;
   title: string;
   completed: boolean;
   subTasks: Todo[];
+}
+
+interface User {
+  username: string | undefined;
+  password: string | undefined;
+  name: string;
+  todos: Todo[];
+  token?: string | null;
+}
+interface AuthContextType {
+  user: User | null;
+  login: (username: string, password: string) => void;
+  logout: () => void;
 }
 
 interface AuthProviderProps {
@@ -32,15 +33,33 @@ export const AuthContext = createContext<AuthContextType>({
   logout: () => {}
 });
 
+
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState<User | null>(null);
+
+  const checkUserCredentials = (username: string, password: string): User | undefined => {
+    const user = usersData.users.find((user) => user.username === username && user.password === password);
+    return user;
+  };
 
   const login = (username: string, password: string) => {
     // Perform authentication logic here
     // Validate username and password from mock data or API
     // Set user information if authentication is successful
 
-    const token = jwt.sign({ username }, 'your-secret-key');
+    const userData = checkUserCredentials(username, password);
+
+    if(userData){
+      const token = jwt.sign({ username }, 'your-secret-key');
+
+      const updatedUser: User = { ...userData, token };
+
+      setUser(updatedUser);
+      localStorage.setItem('user', JSON.stringify(updatedUser));
+    }
+    else{
+
+    }
   };
 
   const logout = () => {
